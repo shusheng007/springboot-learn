aca564c463e4cef5b928a2a314172b71c537474b
Ben.Wang <Ben.Wang@homecreditcfc.cn>
2021-06-26 17:37:06 +0800

    add file handle demo

--- src/main/java/top/ss007/springlearn/controlles/MyController.java
+++ src/main/java/top/ss007/springlearn/controlles/MyController.java
@@ -1,14 +1,17 @@
 package top.ss007.springlearn.controlles;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import top.ss007.springlearn.config.InfoConfig;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import top.ss007.springlearn.model.UserModel;
+import top.ss007.springlearn.service.FileService;
+import top.ss007.springlearn.utils.Base64ToMultipartFile;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Ben.Wang
@@ -20,10 +23,12 @@ import java.util.List;
 @RestController()
 public class MyController {
     private final UserModel userModel;
+    private final FileService fileService;
 
     @Autowired
-    public MyController(UserModel userModel) {
+    public MyController(UserModel userModel, FileService fileService) {
         this.userModel = userModel;
+        this.fileService = fileService;
     }
 
     @GetMapping(value = "/name")
@@ -39,13 +44,30 @@ public class MyController {
     }
 
     @GetMapping(value = "/check")
-    public String isSameObj(){
+    public String isSameObj() {
         return userModel.isSameObj();
     }
 
     @GetMapping(value = "/filter")
-    public String testFilter(@RequestParam(name = "name",required = false)String name){
+    public String testFilter(@RequestParam(name = "name", required = false) String name) {
         return String.valueOf(name);
+    }
+
+    @PostMapping(value = "/uploadFile")
+    public Boolean uploadFile(@RequestBody Map<String, String> request) {
+        String[] base64Array = request.get("base64").split(",");
+        String dataUir, data;
+        if (base64Array.length > 1) {
+            dataUir = base64Array[0];
+            data = base64Array[1];
+        } else {
+            //根据你base64代表的具体文件构建
+            dataUir = "data:image/jpg;base64";
+            data = base64Array[0];
+        }
+        MultipartFile multipartFile = new Base64ToMultipartFile(data, dataUir);
+        fileService.uploadFile(multipartFile);
+        return true;
     }
 
 
