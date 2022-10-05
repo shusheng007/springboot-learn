e84abe0f096d7cc7a762a61c66f462390a14b826
benwang <benwang@linkdoc.com>
2022-10-05 10:47:04 +0800

    feature：增加spring mvc流程解析demo

---
+++ mvc-inspect/src/main/java/top/shusheng007/mvcinspect/MvcInspectApplication.java
@@ -0,0 +1,13 @@
+package top.shusheng007.mvcinspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcInspectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcInspectApplication.class, args);
+    }
+
+}
