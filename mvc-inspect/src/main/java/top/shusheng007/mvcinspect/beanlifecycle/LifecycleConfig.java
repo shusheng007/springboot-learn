d4763f672031e54cec26d0e7a79b244cdbceced7
benwang <benwang@linkdoc.com>
2022-10-06 20:46:33 +0800

    feature：增加spring bean生命周期及扩展点的demo

---
+++ mvc-inspect/src/main/java/top/shusheng007/mvcinspect/beanlifecycle/LifecycleConfig.java
@@ -0,0 +1,23 @@
+package top.shusheng007.mvcinspect.beanlifecycle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by shusheng007
+ *
+ * @author benwang
+ * @date 2022/10/6 15:58
+ * @description:
+ */
+
+@Configuration
+public class LifecycleConfig {
+
+//    @Bean(name = "programmer", initMethod = "myInit2", destroyMethod = "myDestroy2")
+//    public Programmer programmer() {
+//        Programmer programmer = new Programmer();
+//        programmer.setJavaLang(new JavaLang());
+//        return programmer;
+//    }
+}
