66c0ee610bcb98754f15b8cd39a53b00e1147c2b
shusheng007 <shusheng007wb@gmail.com>
2023-03-25 17:21:49 +0800

    feature: refactor

--- mapstruct-demo/src/main/java/top/ss007/usefaulltools/service/ProgramerService.java
+++ mapstruct-demo/src/main/java/top/ss007/usefaulltools/service/ProgramerService.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import top.ss007.usefaulltools.model.*;
-import top.ss007.usefaulltools.model.convetor.ClzProgramerConvertor;
 import top.ss007.usefaulltools.model.convetor.HumanConvertor;
 import top.ss007.usefaulltools.model.convetor.ProgramerConvetor;
 
