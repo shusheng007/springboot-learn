0cd06517ecced826f361e2291dc96f34807223ac
shusheng007 <shusheng007wb@gmail.com>
2024-03-06 16:35:04 +0800

    get git commit and build info

---
+++ composite/src/main/java/top/shusheng007/composite/aop/AopRequest.java
@@ -0,0 +1,9 @@
+package top.shusheng007.composite.aop;
+
+import lombok.Data;
+
+@Data
+public class AopRequest {
+    private String name;
+    private Integer age;
+}
---
+++ composite/src/main/java/top/shusheng007/composite/aop/Student.java
@@ -0,0 +1,11 @@
+package top.shusheng007.composite.aop;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private Integer age;
+}
